5a751de9db01a0e7bee4d2dff819fbf1d5ccfaa2
WALTERSUEL <54998295+WALTERSUEL@users.noreply.github.com>
2020-08-27 20:41:42 -0500

    Add files via upload

---
+++ TrabajoGrupal07/PE2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int info;
+    struct node *left, *right;
+};
+struct node *createnode(int key)
+{
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->info = key;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return(newnode);
+}
+static int count = 0;
+int countnodes(struct node *root)
+{
+    if(root != NULL)
+    {
+        countnodes(root->left);
+        count++;
+        countnodes(root->right);
+    }
+    return count;
+}
+
+int main()
+{
+    //	Creando un arbol. 
+    struct node *newnode = createnode(25);
+    newnode->left = createnode(27);
+    newnode->right = createnode(19);
+    newnode->left->left = createnode(17);
+    newnode->left->right = createnode(91);
+    newnode->right->left = createnode(13);
+    newnode->right->right = createnode(55);
+    /* 
+     *                25
+     *             /    \
+     *            27     19
+     *           / \     / \
+     *         17  91   13 55
+     */
+    printf("Numero de nodos = %d ",countnodes(newnode));
+    printf("\n");
+    count = 0;
+ 
+    return 0;
+}
+
